1b11d928427401273ba024d179abf119b9b78a95
Sabir Baloch <baloch_jan01@live.com>
2018-12-17 05:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string){
 function whisper(string){
   return string.toLowerCase()
 }
-function longShout(hello){
-  console.log(hello.toUpperCase())
+function longShout(string){
+  console.log(string.toUpperCase())
 }
